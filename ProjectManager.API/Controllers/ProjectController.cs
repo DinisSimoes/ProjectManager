8e5725885d10b4feaad43287311ffcd03e5f914d
DinisSimoes <di.f.simoes@gmail.com>
2025-06-04 09:08:30 -0300

    add: controller

---
+++ ProjectManager.API/Controllers/ProjectController.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManager.Application.Projects.Commands;
+using ProjectManager.Application.Projects.Queries;
+
+namespace ProjectManager.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProjectController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public ProjectController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet("user/{userId:guid}")]
+        public async Task<IActionResult> GetProjectsByUser(GetProjectsByUserQuery request)
+        {
+            var result = await _mediator.Send(request);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProjectCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(GetProjectByIdQuery query)
+        {
+            var result = await _mediator.Send(query);
+            return Ok(result);
+        }
+    }
+}
