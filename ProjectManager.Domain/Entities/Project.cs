60f8a42d8d87520cbec8d29561d48673272ca657
DinisSimoes <di.f.simoes@gmail.com>
2025-06-04 09:07:46 -0300

    fix: entities

--- ProjectManager.Domain/Entities/Project.cs
+++ ProjectManager.Domain/Entities/Project.cs
@@ -4,7 +4,8 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public Guid OwnerUserId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
         public List<Task> Tasks { get; set; } = new();
     }
 }
