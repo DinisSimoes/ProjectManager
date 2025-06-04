ee982bd642dfab33360cc9767fbb7d0de18de9fb
DinisSimoes <di.f.simoes@gmail.com>
2025-06-04 09:07:14 -0300

    add: commands, handlers, queries e responses

---
+++ ProjectManager.Application/Projects/Queries/GetProjectByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using ProjectManager.Application.Projects.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Projects.Queries
+{
+    public class GetProjectByIdQuery : IRequest<ProjectResponse>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ ProjectManager.Application/Projects/Responses/ProjectResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.Projects.Responses
+{
+    public class ProjectResponse
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
