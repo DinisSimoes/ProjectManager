4e6e341d83046b6c921ec48636fc889418af29cc
DinisSimoes <di.f.simoes@gmail.com>
2025-06-05 18:17:44 -0300

    update: coloquei mensagem de erro generica

--- ProjectManager.Application/Projects/Handlers/GetProjectsByUserHandler.cs
+++ ProjectManager.Application/Projects/Handlers/GetProjectsByUserHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using ProjectManager.Application.Projects.Queries;
 using ProjectManager.Application.Projects.Responses;
+using ProjectManager.Domain.Entities;
 using ProjectManager.Domain.Interface;
 
 namespace ProjectManager.Application.Projects.Handlers
@@ -17,6 +18,10 @@ namespace ProjectManager.Application.Projects.Handlers
         public async Task<List<ProjectResponse>> Handle(GetProjectsByUserQuery request, CancellationToken cancellationToken)
         {
             var projects = await _projectRepository.GetByUserIdAsync(request.UserId);
+
+            if (projects.Count == 0)
+                throw new KeyNotFoundException("Sem projetos encontrados.");
+
             return projects.Select(p => new ProjectResponse
             {
                 Id = p.Id,
