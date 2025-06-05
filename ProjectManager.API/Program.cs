f88b834731f68dd00a66131457581cd40777ff2b
DinisSimoes <di.f.simoes@gmail.com>
2025-06-05 18:18:37 -0300

    add: dependencias de mediatR, handlers e midlleware

--- ProjectManager.API/Program.cs
+++ ProjectManager.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using ProjectManager.API.Middlewares;
+using ProjectManager.Application.Projects.Handlers;
+using ProjectManager.Domain.Interface;
 using ProjectManager.Infrastructure.Data;
+using ProjectManager.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +17,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(CreateProjectHandler).Assembly);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +35,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
