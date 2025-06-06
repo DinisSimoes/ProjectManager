778b0db5b242954c398b11a802feb81b5c528da7
DinisSimoes <di.f.simoes@gmail.com>
2025-06-06 16:59:57 -0300

    add: CQRS Commands

---
+++ ProjectManager.Application/Tasks/Commands/CreateTaskCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using ProjectManager.Domain.Enums;
+using System.Text.Json.Serialization;
+
+namespace ProjectManager.Application.Tasks.Commands
+{
+    public class CreateTaskCommand : IRequest<Domain.Entities.Task>
+    {
+        [JsonIgnore]
+        public Guid ProjectId { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime DueDate { get; set; }
+        public TaskPriority Priority { get; set; }
+        public Guid ResponsibleUserId { get; set; }
+    }
+}
