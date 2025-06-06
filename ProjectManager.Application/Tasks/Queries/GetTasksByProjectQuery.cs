19161bd20b43a1f1c08668e2f45321b70e8d93cd
DinisSimoes <di.f.simoes@gmail.com>
2025-06-06 17:01:00 -0300

    add: CQRS queries

---
+++ ProjectManager.Application/Tasks/Queries/GetTasksByProjectQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ProjectManager.Application.Tasks.Queries
+{
+    public class GetTasksByProjectQuery : IRequest<IEnumerable<Domain.Entities.Task>>
+    {
+        public Guid ProjectId { get; set; }
+    }
+}
