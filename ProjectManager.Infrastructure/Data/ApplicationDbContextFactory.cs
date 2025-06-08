55f1169b76d79c55bff61b3aa7eb5f1105e2b3f9
DinisSimoes <di.f.simoes@gmail.com>
2025-06-08 00:25:05 -0300

    add: migrations e inclusao da tabela usuarios no context para as migrations

---
+++ ProjectManager.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ProjectManager.Infrastructure.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            // Configure a string de conexão aqui ou pegue de variável de ambiente
+            optionsBuilder.UseNpgsql("Host=projectmanager_db;Database=projectdb;Username=postgres;Password=postgres");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
