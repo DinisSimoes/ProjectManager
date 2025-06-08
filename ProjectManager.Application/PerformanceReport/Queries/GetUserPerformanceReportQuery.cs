3c3cbd6fa01cce646490e73b3ee148e1074feeb3
DinisSimoes <di.f.simoes@gmail.com>
2025-06-08 00:25:34 -0300

    add: CQRS query

---
+++ ProjectManager.Application/PerformanceReport/Queries/GetUserPerformanceReportQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using ProjectManager.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Application.PerformanceReport.Queries
+{
+    public class GetUserPerformanceReportQuery : IRequest<List<UserPerformanceReportDto>>
+    {
+        public Guid RequestingUserId { get; set; }
+    }
+}
