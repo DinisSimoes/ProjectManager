f4d3fd86936df189182c0302a9306a52537cb52d
DinisSimoes <di.f.simoes@gmail.com>
2025-06-08 00:23:26 -0300

    add: repo e interface - performance e ususarios

---
+++ ProjectManager.Domain/Interface/IPerformanceReportRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Interface
+{
+    public interface IPerformanceReportRepository
+    {
+        Task<Dictionary<Guid, double>> GetAverageCompletedTasksPerUserLast30DaysAsync(CancellationToken cancellationToken);
+    }
+}
